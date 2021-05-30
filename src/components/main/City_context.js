e008bb67c3c3967f35387d1846a7113e9e4085cb
Nguyễn Văn Hoà <hoanv.ptit@gmail.com>
2021-05-30 21:06:27 +0700

    ccc

--- src/components/main/City_context.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const City_context = React.createContext(null);
-export default City_context;
--- src/components/utils/cityStorage.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_CITIES = "CITIES";
-export default {
-    getCity() {
-        return JSON.parse(localStorage.getItem(STORAGE_CITIES)) || "";
-    },
-    setCity(city) {
-        localStorage.setItem(STORAGE_CITIES, JSON.stringify(city));
-    }
-}
